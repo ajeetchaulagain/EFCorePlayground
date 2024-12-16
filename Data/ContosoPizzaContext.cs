ecdd08a8c8c1bdfa3043392145e10b6f4de55ae2
ajeetchaulagain <ajeet.chaulagain@endava.com>
2024-12-16 16:53:40 +1100

    style: cleanup unused constructer initialization

--- Data/ContosoPizzaContext.cs
+++ Data/ContosoPizzaContext.cs
@@ -14,15 +14,6 @@ public class ContosoPizzaContext: DbContext
     public DbSet<Product> Products { get; set; } = null!;
     public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
 
-    public string DbPath { get; }
-
-    public ContosoPizzaContext()
-    {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "contoso-pizza.db");
-    }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ContosoPizza-Part1;Integrated Security=True;");
